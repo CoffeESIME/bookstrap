7bf88ed312ae72396699fc74031207b853b60827
Fabian.Romero <fabian.romero@xylem.com>
2023-12-21 13:47:16 -0600

    environment

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -3,17 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Book } from '../models/book.model';
 import { ApiResponse } from '../models/api.model';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BooksService {
-  private url: string = 'https://www.dbooks.org/api/search/python';
   private data$: Observable<Book[]> | null = null;
   constructor(private http: HttpClient) {}
   getData(): Observable<Book[]> {
     if (!this.data$) {
-      this.data$ = this.http.get<ApiResponse>(this.url).pipe(
+      this.data$ = this.http.get<ApiResponse>(environment.apiUrl).pipe(
         map((response) =>
           response.books.map((book, index) =>
             index % 3 === 0
