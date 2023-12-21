004ad642a80563e54481e2008bdfe81bc860f1f7
Fabian.Romero <fabian.romero@xylem.com>
2023-12-21 16:21:07 -0600

    reading now component

---
+++ src/app/features/reading-now/reading-now.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+
+import { BookCardComponent } from './book-card.component';
+import { CommonModule } from '@angular/common';
+import { BooksService } from '../../services/books.service';
+import { Book } from '../../models/book.model';
+
+@Component({
+  selector: 'app-reading-now',
+  standalone: true,
+  templateUrl: './reading-now.component.html',
+  styleUrls: ['./reading-now.component.css'],
+  imports: [BookCardComponent, CommonModule],
+})
+export class ReadingNowComponent implements OnInit {
+  public dataBooks: Book[] | null = null;
+  constructor(
+    private dataService: BooksService,
+    private cd: ChangeDetectorRef
+  ) {}
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(
+      (data) => {
+        this.dataBooks = data;
+        this.cd.detectChanges();
+      },
+      (error) => console.error(error)
+    );
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { UserComponent } from '../../features/user/user.component';
 import { BookshelfComponent } from '../../features/bookshelf/bookshelf.component';
+import { ReadingNowComponent } from '../../features/reading-now/reading-now.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, UserComponent, BookshelfComponent],
+  imports: [
+    CommonModule,
+    UserComponent,
+    BookshelfComponent,
+    ReadingNowComponent,
+  ],
   templateUrl: `./home.component.html`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
