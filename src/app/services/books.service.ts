87129d07c670a0d9ae4344a92797244f53fdf3ec
Fabian.Romero <fabian.romero@xylem.com>
2023-12-20 22:53:32 -0600

    no comments

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -23,7 +23,7 @@ export class BooksService {
                   users_choice: true,
                   status: 'Reading',
                   percentage: Math.random() * 100,
-                  genre: 'Engineer',
+                  genre: 'Science',
                 }
               : {
                   ...book,
@@ -31,7 +31,7 @@ export class BooksService {
                   users_choice: false,
                   status: 'Read',
                   percentage: Math.random() * 100,
-                  genre: 'Engineer',
+                  genre: 'Science',
                 }
           )
         )
@@ -40,46 +40,3 @@ export class BooksService {
     return this.data$;
   }
 }
-
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable, shareReplay, tap, map } from 'rxjs';
-// import { Book } from '../models/book.model';
-
-// export class DataService {
-//   private url: string = 'https://www.dbooks.org/api/search/python';
-//   private data$?: Observable<Book[]>;
-
-//   constructor(private http: HttpClient) {}
-
-//   getData(): Observable<Book[]> {
-//     if (!this.data$) {
-//       this.data$ = this.http.get<ApiResponse>(this.url).pipe(
-//         map((response) =>
-//           response.books.map((book, index) =>
-//             index % 3 === 0
-//               ? {
-//                   ...book,
-//                   price: Math.random() * 12,
-//                   users_choice: true,
-//                   status: 'Reading',
-//                   percentage: Math.random() * 100,
-//                   genre: 'Engineer',
-//                 }
-//               : {
-//                   ...book,
-//                   price: Math.random() * 12,
-//                   users_choice: false,
-//                   status: 'Read',
-//                   percentage: Math.random() * 100,
-//                   genre: 'Engineer',
-//                 }
-//           )
-//         ),
-//         tap((books) => console.log('Books:', books)),
-//         shareReplay(1)
-//       );
-//     }
-//     return this.data$;
-//   }
-// }
