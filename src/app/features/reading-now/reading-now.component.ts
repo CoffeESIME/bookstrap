e93598904051671800dcf23389c21539b2874db5
Fabian.Romero <fabian.romero@xylem.com>
2024-01-01 21:38:47 -0600

    refactoring components dump and smart components

--- src/app/features/reading-now/reading-now.component.ts
+++ src/app/features/reading-now/reading-now.component.ts
@@ -1,30 +1,23 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-
 import { BookCardComponent } from './book-card.component';
 import { CommonModule } from '@angular/common';
-import { BooksService } from '../../services/books.service';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../../models/book.model';
+import { BooksService } from '../../services/books.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reading-now',
-  standalone: true,
   templateUrl: './reading-now.component.html',
   styleUrls: ['./reading-now.component.css'],
+  standalone: true,
   imports: [BookCardComponent, CommonModule],
 })
 export class ReadingNowComponent implements OnInit {
-  public dataBooks: Book[] =[];
-  constructor(
-    private dataService: BooksService,
-    private cd: ChangeDetectorRef
-  ) {}
+  public dataBooks$!: Observable<Book[]> ;
+
+  constructor(private dataService: BooksService) {}
+
   ngOnInit(): void {
-    this.dataService.getData().subscribe(
-      (data) => {
-        this.dataBooks = data;
-        this.cd.detectChanges();
-      },
-      (error) => console.error(error)
-    );
+    this.dataBooks$ = this.dataService.getData();
   }
 }
